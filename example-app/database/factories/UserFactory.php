865703e5f7ab75e7fba22010b1d9787d84193b66
MateusCampaner <mateus.campaner@edu.unifil.br>
2023-05-30 21:29:03 -0300

    teste4

--- example-app/database/factories/UserFactory.php
+++ example-app/database/factories/UserFactory.php
@@ -15,7 +15,7 @@ class UserFactory extends Factory
      * Define the model's default state.
      *
      * @return array<string, mixed>
-     */#######
+     */
     public function definition(): array
     {
         return [
